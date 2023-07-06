9b40587ff41fc4245e92bca5d56f75c7a7f02549
Jon Moss <joth.moss@googlemail.com>
2023-07-06 15:54:48 +0100

    Updated CLI - now builds routes

--- vstp/vstp.py
+++ vstp/vstp.py
@@ -53,6 +53,23 @@ class TiplocTable():
         
         for row in results:
             self.table.add_row(*row.split(':'))
+class LinkSelectTable():
+    """"""
+    def __init__(self, results):
+        ""
+        self.table = Table(
+            show_header=True,
+            header_style="bold magenta",
+            
+        )
+        
+        self.table.add_column('#',)
+        self.table.add_column('TIPLOC')
+        self.table.add_column('Name')
+        
+        for row in results:
+
+            self.table.add_row(*row)
 
 psr = argparse.ArgumentParser(
     prog='vstp',
@@ -69,7 +86,9 @@ psr.add_argument(
     default=False,
     help='Show output as grouped legs between via TIPLOCS'
 )
-psr.add_argument('--find', help='find TIPLOC')
+psr.add_argument('--from_loc', type=str, help ='from <TIPLOC> show all linked locations')
+psr.add_argument('--find', type=str, help='find TIPLOC')
+psr.add_argument('--build', type=str, help='Build a route, provide the first TIPLOC')
 args = psr.parse_args()
 
 os.system('clear')
@@ -83,6 +102,73 @@ if args.find:
     results = LocationRecord.match_locations(args.find)
     table = TiplocTable(results)
     CONSOLE.print(table.table)
+    sys.exit(0)
+    
+if args.from_loc:
+    CONSOLE.print(Markdown(f"# Network Links: ```{args.from_loc}```"))
+    results = f_import.NetworkLink.get_neighbours(args.from_loc)
+    locs = []
+    for result in results:
+        rcd = LocationRecord._instances[result]
+        locs.append(f'{rcd.location_code}:{rcd.location_name}')
+    table = TiplocTable(locs)
+    CONSOLE.print(table.table)
+    sys.exit(0)
+    
+if args.build:
+    
+    links = []
+    
+    def trip_table(cur_trip: list) -> str:
+        locs = []
+        for result in cur_trip:
+            rcd = LocationRecord._instances[result]
+            locs.append(f'{rcd.location_code}:{rcd.location_name}')
+        return TiplocTable(locs).table
+    
+    def get_links(tiploc: str) -> str:
+        results = f_import.NetworkLink.get_neighbours(tiploc)
+        locs = []
+        for index, result in enumerate(results):
+            rcd = LocationRecord._instances[result]
+            locs.append([str(index + 1), rcd.location_code, rcd.location_name])
+        return locs
+    
+    current_trip = []
+    if not LocationRecord.return_instance(args.build):
+        CONSOLE.print(Markdown(f"# ERROR, invalid TIPLOC: ```{args.build}```"))
+        sys.exit(1)
+    current_trip.append(args.build)
+    while True and current_trip:
+        CONSOLE.clear()
+        if len(current_trip) > 1:
+            CONSOLE.print(Markdown(f"# Route Builder, START: ```{current_trip[0]}```, END: ```{current_trip[-1]}```"))
+        else:
+            CONSOLE.print(Markdown(f"# Route Builder, START: ```{args.build}```"))
+        CONSOLE.print(Markdown('## Current Route'))
+        CONSOLE.print(trip_table(current_trip))
+        links = get_links(current_trip[-1])
+        CONSOLE.print(Markdown('## Next Location Options'))
+        print()
+        table = LinkSelectTable(links).table
+        CONSOLE.print(table)
+        print()
+        answer = CONSOLE.input(Markdown(f"# [#] Add TIPLOC from table | [R]emove last entry | e[X]it: "))
+
+        if answer == "X":
+            CONSOLE.clear()
+            if len(current_trip) > 1:
+                CONSOLE.print(Markdown(f"# Route Builder, START: ```{current_trip[0]}```, END: ```{current_trip[-1]}```"))
+            else:
+                CONSOLE.print(Markdown(f"# Route Builder, START: ```{args.build}```"))
+                CONSOLE.print(Markdown('## Current Route'))
+            CONSOLE.print(trip_table(current_trip))
+            sys.exit(0)
+        if answer == "R" and current_trip:
+            current_trip.pop()
+        if answer.isnumeric():
+            answer = int(answer) - 1
+            current_trip.append(links[answer][1])
     sys.exit(0)
 
 via = []
